f54fc1acd3b7646b8df8b838700bae136badcf6c
lim <lim@lim-pc>
2016-12-18 16:36:20 +0900

    .

--- NHNent/src/dao/DAO.java
+++ NHNent/src/dao/DAO.java
@@ -1,11 +1,18 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 //DB의 CRUD기능을 위한 클래스
 public class DAO {
-			private DAO() {}
 			private static DAO dao;
+			private Connection conn = null;
+			private DAO() {
+						conn = DriverManager.getConnection(url)
+			}
 			
 			//싱글톤을 위한 함수
 			public DAO getInstancd(){
@@ -15,8 +22,15 @@ public class DAO {
 						return dao;
 			}
 			
-			public void insert(){
+			public void insert() throws SQLException{
 						PreparedStatement pstmt = null;
+						try{
+									String sql = "insert into geustbook_table values(?,?,?)";
+									pstmt = conn.prepareStatement(sql);
+									pstmt.setString(1, x);
+						}finally {
+									
+						}
 			}
 			public void select(){
 						
--- NHNent/src/model/Message.java
+++ NHNent/src/model/Message.java
@@ -7,5 +7,35 @@ public class Message {
 			private String password;
 			private String contents;
 			private long time;
+			public int getId() {
+						return id;
+			}
+			public void setId(int id) {
+						this.id = id;
+			}
+			public String getUser() {
+						return user;
+			}
+			public void setUser(String user) {
+						this.user = user;
+			}
+			public String getPassword() {
+						return password;
+			}
+			public void setPassword(String password) {
+						this.password = password;
+			}
+			public String getContents() {
+						return contents;
+			}
+			public void setContents(String contents) {
+						this.contents = contents;
+			}
+			public long getTime() {
+						return time;
+			}
+			public void setTime(long time) {
+						this.time = time;
+			}
 			
 }
